9f4ca2f1f785453f05eac0e1c8278764050a9d61
ehsankhayambashi <khayambashi.ehsan@gmail.com>
2022-07-13 15:20:38 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const data = [
+  { row: ["1", "2", "3"] },
+  { row: ["1", "2", "3", "4"] },
+  { row: ["1", "2"] },
+];
+let initialState = {};
+
+data.map((row, rowIndex) => {
+  row.row.map((column, columnIndex) => {
+    initialState[`${rowIndex}-${columnIndex}`] = false;
+  });
+});
 
 function App() {
+  const [state, setState] = useState(initialState);
+
+  const handleClose = (e) => {
+    let { id } = e.currentTarget;
+    setState((prevState) => {
+      return {
+        ...prevState,
+        [id]: true,
+      };
+    });
+  };
+
+  const handleReset = (e) => {
+    const { id } = e.currentTarget;
+    let counter = -1;
+    let resetState = { ...state };
+    for (const property in resetState) {
+      if (property.startsWith(id.toString() + "-")) {
+        counter = counter + 1;
+        resetState[id + "-" + counter] = false;
+      }
+    }
+    setState(resetState);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {data.map((row, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          <div className="d-flex">
+            <div>
+              <button id={rowIndex} onClick={(e) => handleReset(e)}>
+                reset
+              </button>
+            </div>
+            {row.row.map((number, columnIndex) => (
+              <div
+                className={
+                  state[rowIndex.toString() + "-" + columnIndex.toString()]
+                    ? "col d-flex justify-content-between border bg-success text-white d-none"
+                    : "col d-flex justify-content-between border bg-success text-white"
+                }
+                key={columnIndex}
+              >
+                {number}
+                <button
+                  id={`${rowIndex}-${columnIndex}`}
+                  onClick={(e) => handleClose(e)}
+                >
+                  close
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
